8793b94234052482e4ef106a5292a0d03f96a16d
Viacheslav-ua <atph@ukr.net>
2023-10-24 11:31:25 +0300

    added styles for PostItem component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import PostItem from "./components/PostItem";
 
 function App() {
 
 
   return (
-    <div>
-      <Counter />
-      <ClassCounter />
+    <div className="App">
+      <div>
+        <Counter />
+        <ClassCounter />
+        <PostItem />
+      </div>
     </div>
+
   );
 }
 
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,18 @@
+import '../styles/app.css'
+
+const PostItem = () => {
+
+  return (
+  <div className="post">
+    <div className="post__content">
+      <strong>1. Javascript</strong>
+      <div>JavaScript - programming language</div>
+    </div>
+    <div className="post__btn">
+      <button>delete</button>
+    </div>
+    </div>
+  );
+};
+
+export default PostItem;
