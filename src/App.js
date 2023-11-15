223354e8111a1cbf315e3a343bce8acc17623396
Viacheslav-ua <atph@ukr.net>
2023-11-15 19:43:50 +0200

    sorting implemented

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UA/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: 'JavaScript', content: 'Programming language' },
     { id: 2, title: 'Postgres', content: 'Data base' },
   ])
+
+  const [selectedSort, setSelectedSort] = useState('')
 
   const createPost = (newPost) => {
     setPosts([
@@ -21,6 +24,11 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const sortPosts = (sort) => {
+    console.log(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
+
 
 
   return (
@@ -28,6 +36,19 @@ function App() {
       <PostForm create={createPost} />
       <Counter />
       <ClassCounter />
+
+      <div>
+        <hr style={{ margin: '15px 8px' }} />
+        <MySelect
+          defaultValue="Order by"
+          value={selectedSort}
+          onChange={sortPosts}
+          options={[
+            { value: 'title', name: 'By name' },
+            { value: 'content', name: 'By description' },
+          ]}
+        />
+      </div>
 
       {posts.length !== 0
         ? <PostList remove={removePost} posts={posts} title={'List of posts'} />
