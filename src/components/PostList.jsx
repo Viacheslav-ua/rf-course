6659ae1c7c2e3e0c0faa1a7b1dd9e9b7e0279220
Viacheslav-ua <atph@ukr.net>
2023-11-16 21:50:09 +0200

    react-transition-group

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,3 +1,4 @@
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import PostItem from "./PostItem";
 
 function PostList({ posts, title, remove }) {
@@ -12,14 +13,25 @@ function PostList({ posts, title, remove }) {
 
   return (
     <div>
-      <h1 style={{ textAlign: 'center' }}>{ title }</h1>
+      <h1 style={{ textAlign: 'center' }}>{title}</h1>
+      <TransitionGroup>
         {
-        posts.map((post, index) => <PostItem
-          remove={remove}
-          number={index + 1}
-          post={post}
-          key={post.id} />)
+          posts.map((post, index) =>
+            <CSSTransition
+              key={post.id}
+              timeout={500}
+              classNames="post"
+            >
+              <PostItem
+              remove={remove}
+              number={index + 1}
+              post={post}
+              />
+            </CSSTransition>
+          )
         }
+      </TransitionGroup>
+
     </div>
   );
 }
