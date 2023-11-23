c1a4f6f9a25cb2ec60e05e9626fdca062b70b457
Viacheslav-ua <atph@ukr.net>
2023-11-23 13:14:28 +0200

    Added comments to post

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,15 +7,21 @@ import Loader from "../components/UA/loader/loader"
 
 const PostIdPage = () => {
   const [post, setPost] = useState({})
+  const [comments, setComments] = useState([])
   const navigate = useNavigate()
   const params = useParams()
-  const [fetchPostById, isLoading, error] = useFetching(async () => {
+  const [fetchPostById, isLoading, postError] = useFetching(async () => {
     const response = await PostService.getById(params.id)
     setPost(response.data)
+  })
+  const [fetchComments, isComLoading, comError] = useFetching(async () => {
+    const response = await PostService.getCommentByPostId(params.id)
+    setComments(response.data)
   })
 
   useEffect(() => {
     fetchPostById(params.id)
+    fetchComments(params.id)
   }, [])
 
   return (
@@ -25,8 +31,22 @@ const PostIdPage = () => {
       {isLoading 
         ? <Loader />
         : <div>
-          <h1>{post.title}</h1>
+          <h2>{post.title}</h2>
           <p>{post.body}</p>
+        </div>
+      }
+      <h2 style={{marginTop: 40}}>Comments</h2>
+      {isComLoading
+        ? <Loader />
+        : <div>
+          {comments.map(comment => 
+            <div style={{marginTop: 15}}>
+              <h5>{comment.email}</h5>
+              <p>
+                {comment.body}
+              </p>
+            </div>
+          )}
         </div>
       }
     </div>
