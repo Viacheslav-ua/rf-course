bbfba54273e0d9a94da6153cd90349ff74929582
Viacheslav-ua <atph@ukr.net>
2023-11-23 12:17:37 +0200

    useNavigate hook

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,14 +1,34 @@
 import { useNavigate, useParams } from "react-router-dom"
 import MyButton from "../components/UA/button/MyButton"
+import { useEffect, useState } from "react"
+import { useFetching } from "../hooks/useFetching"
+import PostService from "../API/postService"
+import Loader from "../components/UA/loader/loader"
 
 const PostIdPage = () => {
+  const [post, setPost] = useState({})
   const navigate = useNavigate()
-  const { id } = useParams()
+  const params = useParams()
+  const [fetchPostById, isLoading, error] = useFetching(async () => {
+    const response = await PostService.getById(params.id)
+    setPost(response.data)
+  })
+
+  useEffect(() => {
+    fetchPostById(params.id)
+  }, [])
 
   return (
     <div>
       <MyButton onClick={() => navigate(-1)}>Go Back</MyButton>
-      <h1>Page of post with id - {id}</h1>
+      <h1>Page of post with id - {params.id}</h1>
+      {isLoading 
+        ? <Loader />
+        : <div>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      }
     </div>
   )
 }
